8aed4ec963aea0298bdcf20f4baa74ecf9f60c3b
Landrodie <nilsland31@gmail.com>
2021-04-07 23:16:53 +0200

    Fetching data from URL

--- tweetviz/src/App.js
+++ tweetviz/src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
-import { range } from 'd3';
-import { Face } from './TweetViz/Face';
 
-const width = 166
-const height = 166
 
-const array = range (7 * 2)
+const App = ()  => {
+ 
+  // const promise = fetch(url);
+  // promise.then(response =>{
+  //   response.text().then(text =>{
+  //     console.log(text)
+  //   })
+  // })
+  const fetchText = async (url) => {
+    const response = await fetch(url);
+    return await response.text();
+  }
 
-const App = ()  => array.map(() => (
-      <Face
-        width = {width}
-        height = {height}
-        centerX = {width / 2}
-        centerY = {height / 2}
-        strokeWidth = {10}
-        eyeOffsetX = {15 + Math.random() * 20 }
-        eyeOffsetY = {20 + Math.random() * 15}
-        eyeRadius = {5 + Math.random() * 10}
-        mouthWidth = {7 + + Math.random() * 9}
-        mouthRadius = {30 + Math.random() * 10}
-      />
-));
+  const csvUrl = "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv"
+
+  fetchText(csvUrl).then(text =>{
+    console.log(text)
+  })
+
+  return(<div></div>)
+}
 
 export default App;
