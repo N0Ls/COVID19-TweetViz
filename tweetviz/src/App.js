ba6d21d1fc904159b1604bdc506ec1462a778c87
Landrodie <nilsland31@gmail.com>
2021-04-09 00:06:43 +0200

    Adding useState

--- tweetviz/src/App.js
+++ tweetviz/src/App.js
@@ -1,39 +1,27 @@
 import './App.css';
-import { Face } from './TweetViz/Face';
-
+import { useState, useCallback } from 'react'
 
 const height = 960;
 const width = 500;
-const circleX = width / 2;
-const circleY = height / 2;
 const circleRadius = 30
 
-const handleMouseMove = (event) => {
-  const { clientX, clientY } = event;
-  console.log({clientX : clientX, clientY : clientY})
-}
+const initialMousePosition = {x:width/2, y:height/2}
 
-const App = ()  => (
-//  <Face
-//   width = {160} 
-//   height = {160}
-//   centerX = {160/2}
-//   centerY = {160/2}
-//   strokeWidth = {5}
-//   eyeOffsetX = {20}
-//   eyeOffsetY = {35}
-//   eyeRadius = {10}
-//   mouthWidth = {5}
-//   mouthRadius = {30}
-//  />
-
-<svg width={width} height={height} onMouseMove={handleMouseMove}>
-  <circle 
-    cx = {circleX}
-    cy = {circleY}
-    r  = {circleRadius}
-  />
-</svg>
-)
+const App = ()  => {
+  const [mousePosition, setMousePosition] = useState(initialMousePosition);
+  const handleMouseMove = useCallback(event => {
+    const { clientX, clientY } = event;
+    setMousePosition({x: clientX, y : clientY})
+  }, [setMousePosition]);
+  return (
+    <svg width={width} height={height} onMouseMove={handleMouseMove}>
+      <circle 
+        cx = {mousePosition.x}
+        cy = {mousePosition.y}
+        r  = {circleRadius}
+      />
+    </svg>
+  );
+};
 
 export default App;
