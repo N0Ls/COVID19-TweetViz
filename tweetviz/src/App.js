1ce1ebb71a82aae34d7086defc97a7681589bc8d
Landrodie <nilsland31@gmail.com>
2021-04-08 23:47:25 +0200

    Mouse interaction discovery

--- tweetviz/src/App.js
+++ tweetviz/src/App.js
@@ -1,26 +1,39 @@
 import './App.css';
+import { Face } from './TweetViz/Face';
 
 
-const App = ()  => {
- 
-  // const promise = fetch(url);
-  // promise.then(response =>{
-  //   response.text().then(text =>{
-  //     console.log(text)
-  //   })
-  // })
-  const fetchText = async (url) => {
-    const response = await fetch(url);
-    return await response.text();
-  }
+const height = 960;
+const width = 500;
+const circleX = width / 2;
+const circleY = height / 2;
+const circleRadius = 30
 
-  const csvUrl = "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv"
-
-  fetchText(csvUrl).then(text =>{
-    console.log(text)
-  })
-
-  return(<div></div>)
+const handleMouseMove = (event) => {
+  const { clientX, clientY } = event;
+  console.log({clientX : clientX, clientY : clientY})
 }
+
+const App = ()  => (
+//  <Face
+//   width = {160} 
+//   height = {160}
+//   centerX = {160/2}
+//   centerY = {160/2}
+//   strokeWidth = {5}
+//   eyeOffsetX = {20}
+//   eyeOffsetY = {35}
+//   eyeRadius = {10}
+//   mouthWidth = {5}
+//   mouthRadius = {30}
+//  />
+
+<svg width={width} height={height} onMouseMove={handleMouseMove}>
+  <circle 
+    cx = {circleX}
+    cy = {circleY}
+    r  = {circleRadius}
+  />
+</svg>
+)
 
 export default App;
