8b8f70a042bca729313fca30177da34541c47a9e
Landrodie <nilsland31@gmail.com>
2021-05-25 00:21:10 +0200

    Update data

--- tweetviz/src/TweetViz/useData.js
+++ tweetviz/src/TweetViz/useData.js
@@ -1,10 +1,6 @@
 import { useState, useEffect } from 'react'
 import { csv } from 'd3';
-import data from "./Data/1.csv";
-
-import {yScale} from "../App.js"
-
-import App from '../App';
+import data from "./Data/data.csv";
 
 const csvUrl = data
 
@@ -14,18 +10,17 @@ export const useData = () =>{
 
     useEffect(() => {
         const row = d => {
-            d.influenceFactor = Math.log10(1+ ( d.retweet_count* 4.2)) * Math.log10(1+(d.favorite_count*1.2)) * Math.log10(1+(d.user_followers_count/(1+d.user_friends_count)));
             d.formatedDate = new Date(Date.parse(d.created_at));
             return d;
         }
         csv(csvUrl, row).then(data => {
         setData(data.filter(d => 
-            d.influenceFactor > 15
+            parseFloat(d.influenceNumber) > 5 && d.formatedDate.getFullYear() > 2019 && d.type === "news"
         ))
         });
     }, []);
 
-    console.log(data?data:"not loaded")
+    console.log(data?data[0]:"not loaded")
 
     return data;
 }
